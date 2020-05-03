8668d19cab99a4099cfcb9ea35085668117cfe8f
golebiowskipj@gmail.com <golebiowskipj@gmail.com>
2020-05-03 22:09:04 +0200

    Updates

---
+++ precache-manifest.c85bd126ef551a684276028247d60d74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8afdd48716a891d7b72e6835d48819c5",
+    "url": "/shopping-list/index.html"
+  },
+  {
+    "revision": "b4b4100e067eb0eaf60b",
+    "url": "/shopping-list/static/css/main.c9a4084a.chunk.css"
+  },
+  {
+    "revision": "6bacf20bacaa68a97ab8",
+    "url": "/shopping-list/static/js/2.0e9cc765.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping-list/static/js/2.0e9cc765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4b4100e067eb0eaf60b",
+    "url": "/shopping-list/static/js/main.ac98c934.chunk.js"
+  },
+  {
+    "revision": "d9340a41441eaccbc9e2",
+    "url": "/shopping-list/static/js/runtime-main.25963532.js"
+  }
+]);
